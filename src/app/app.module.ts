765dff87feee66448283819b7e6626fbc5f93fb9
Mario Padilla Carrión <mca@expandit.com>
2016-10-21 13:44:52 +0200

    Load polymer elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 import { AppComponent } from './app.component';
 
